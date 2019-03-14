cf156d2291db70cc52b640f59c88ffa3c0c42750
chencheng <1982989993@qq.com>
2019-03-14 20:13:15 +0800

    完善管理员登录登出ManagerLoginController
    1.管理员登录的cookie和session定义保存时间
    	通过md5加密验证密码
    	通过md5生成唯一串处理用户每次请求
    2.管理员登出注销session更新cookie

--- src/main/java/whut/controller/ManagerLoginController.java
+++ src/main/java/whut/controller/ManagerLoginController.java
@@ -1,5 +1,8 @@
 package whut.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,32 +10,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import whut.service.MemberLoginService;
+import whut.service.ManagerLoginService;
 import whut.utils.ResponseData;
 
 @Controller
 @RequestMapping(value = "/manager/login")
 public class ManagerLoginController {
 	@Autowired
-	private MemberLoginService memberLoginService;
+	private ManagerLoginService managerLoginService;
 	
 	@RequestMapping(value = "/in", method = RequestMethod.POST)
-	public @ResponseBody ResponseData loginin(@RequestBody String jsonString) {
-		return  memberLoginService.loginin(jsonString);
+	public @ResponseBody ResponseData loginin(@RequestBody String jsonString, HttpServletRequest request, HttpServletResponse response) {
+		return  managerLoginService.loginin(jsonString, request, response);
 	}
 	
+	
 	@RequestMapping(value = "/out", method = RequestMethod.GET)
-	public @ResponseBody ResponseData loginout(String username) {
-		return  memberLoginService.loginout(username);
+	public @ResponseBody ResponseData loginout(String username, HttpServletRequest request, HttpServletResponse response) {
+		return  managerLoginService.loginout(username, request, response);
 	}
 	
 	@RequestMapping(value = "/getPhoneCode", method = RequestMethod.GET)
 	public @ResponseBody ResponseData getPhoneCode(String phoneCode) {
-		return  memberLoginService.getPhoneCode(phoneCode);
+		return  managerLoginService.getPhoneCode(phoneCode);
 	}
 	
 	@RequestMapping(value = "/getMailCode", method = RequestMethod.GET)
 	public @ResponseBody ResponseData getMailCode(String mailCode) {
-		return  memberLoginService.getMailCode(mailCode);
+		return  managerLoginService.getMailCode(mailCode);
 	}
 }
