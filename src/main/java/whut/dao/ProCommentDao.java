f0ca957e5146c019dbe565578c0b123e62d6c5b5
AlphaWun <1217276968@qq.com>
2019-04-14 09:50:24 +0800

    修改商品评论
    ProductComment POJO中order_id改为order_detail_id
    ProCommentMapper增加getListByCondition方法

--- src/main/java/whut/dao/ProCommentDao.java
+++ src/main/java/whut/dao/ProCommentDao.java
@@ -22,7 +22,7 @@ public interface ProCommentDao {
 	public ProductComment getCommentById(String id);
 
 	//管理员根据商品名称、商品编码、用户名称联合查询
-	public List<ProUserComment> getListByCondition(Map<String, Object> map);
+	public List<ProductComment> getListByCondition(Map<String, Object> map);
 
 
 }
